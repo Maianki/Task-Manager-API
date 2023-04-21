66471d51247f23ffa741fc0b75bae538635cc517
Ankit Kumain <kumankit10@gmail.com>
2023-04-21 20:25:03 +0530

    initial commit

---
+++ db/tasks.js
@@ -0,0 +1,26 @@
+const tasks = [{
+    "id": 1,
+    "title": "Task Manager API",
+    "description": "Assignment to learn CRUD in express",
+    "isCompleted": true
+  }, {
+    "id": 2,
+    "title": "Time Complexity Revision",
+    "description": "Revise time complexity session",
+    "isCompleted": false
+  }, {
+    "id": 3,
+    "title": "Practise DSA",
+    "description": "Practise DSA questions",
+    "isCompleted": false
+  }, {
+    "id": 4,
+    "title": "Install NodeJS and VS Code",
+    "description": "Install NodeJS and VS Code in local system",
+    "isCompleted": true
+  }, {
+    "id": 5,
+    "title": "Submit project",
+    "description": "Submit the projects done so far.",
+    "isCompleted": false
+  }]
